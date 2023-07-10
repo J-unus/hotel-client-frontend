a9e1a8f8e8f004a0e839a8da6e40e36d03fe7233
kristjan.junus <kristjan.junus@helmes.com>
2023-07-10 09:27:43 +0300

    Add basic card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,22 @@ import {UiModule} from '@egov/cvi-ng';
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {SharedModule} from "./shared/shared.module";
+import {CommonModule} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MAT_DATE_FORMATS, MAT_DATE_LOCALE} from "@angular/material/core";
+import {MatMomentDateModule} from "@angular/material-moment-adapter";
+
+const ESTONIAN_DATE_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'DD.MM.YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @NgModule({
   declarations: [
@@ -19,8 +35,15 @@ import {SharedModule} from "./shared/shared.module";
     UiModule,
     HttpClientModule,
     SharedModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatMomentDateModule,
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'et'},
+    {provide: MAT_DATE_FORMATS, useValue: ESTONIAN_DATE_FORMATS},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
