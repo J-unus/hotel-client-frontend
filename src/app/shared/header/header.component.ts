c19bd859e2929ab6387a64bc42a8b99072601968
kristjan.junus <kristjan.junus@helmes.com>
2023-07-14 14:39:35 +0300

    Add logout and login to header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AccountService} from "../../core/auth/account.service";
 import {LoginService} from "../../core/auth/login.service";
 import {StateStorageService} from "../../core/auth/state-storage.service";
+import {Subject, takeUntil} from "rxjs";
+import {AccountDto} from "../../core/auth/account.dto";
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  account: AccountDto | null = null;
+
+  private readonly destroy$ = new Subject<void>();
 
 	constructor(private router: Router,
               private loginService: LoginService,
@@ -17,13 +22,21 @@ export class HeaderComponent {
               private accountService: AccountService) {
   }
 
+  ngOnInit(): void {
+    this.accountService
+      .getAuthenticationState()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(account => (this.account = account));
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
 	navigate(link: string) {
 		this.router.navigate([link]);
 	}
-
-  isAuthenticated(): boolean {
-    return this.accountService.isAuthenticated();
-  }
 
   logout(): void {
     this.loginService.logout();
